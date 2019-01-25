add57d65dd1942479d589d2ac7bc9abedb591f16
ZhangSiFan <451540776@qq.com>
2019-01-26 00:07:28 +0800

    提交Camera作业

---
+++ HOMEWORK_20190125_Camera/Chapter7-camera/app/src/main/java/com/bytedance/camera/demo/MainActivity.java
@@ -0,0 +1,29 @@
+package com.bytedance.camera.demo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.btn_picture).setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, TakePictureActivity.class));
+        });
+
+        findViewById(R.id.btn_camera).setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, RecordVideoActivity.class));
+        });
+
+        findViewById(R.id.btn_custom).setOnClickListener(v -> {
+            //todo 在这里申请相机、麦克风、存储的权限
+            startActivity(new Intent(MainActivity.this, CustomCameraActivity.class));
+
+        });
+    }
+
+}
